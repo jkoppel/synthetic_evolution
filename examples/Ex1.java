4f223c841610cc73bc140d7c8cb2296192a1bf71
James Koppel <jamesbkoppel@yahoo.com>
2015-04-08 23:45:14 -0400

    Checkpoint on examples

---
+++ examples/Ex1.java
@@ -0,0 +1,29 @@
+/*
+ *
+ * v1: 1, field1
+ * v2: 1, field1, 2, field2
+ * v3: 1, field1, 2, field2, 3, field3
+ * v4: 1, field1, 3, field3
+ */
+
+public class DataStruct {
+    int field1;
+    int field3;
+}
+
+//...
+
+public DataStruct read(int[] in) {
+    DataStruct result = new DataStruct();
+    result.field1 = in[1];
+
+    if (in.length > 2 && in[2] == 3) {
+        result.field3 = in[3];
+    } else if (in.length > 4 && in[4] == 3) {
+        result.field3 = in[5];
+    } else {
+        result.field3 = 0;
+    }
+
+    return result;
+}
