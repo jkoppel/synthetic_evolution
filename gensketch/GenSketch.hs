72fe148a54e6cfcfd27c38f2835cb212e8baaff6
Chelsea Voss <csvoss@mit.edu>
2015-04-10 15:49:46 -0400

    GenSketch.hs: structs and file size declaration
    - Reading lines from stdin is working
    - Outputting structs is working
    - Calculating and outputting file size is working
    
    Still TODO -- refactor so that there's an intermediate AST.

--- gensketch/GenSketch.hs
+++ gensketch/GenSketch.hs
@@ -1,3 +1,81 @@
-import Data.SketchFileFormat.Format
+{-# LANGUAGE OverloadedStrings #-}
 
-main = return ()
+import Data.SketchFileFormat.Format
+import Text.PrettyPrint
+
+main :: IO ()
+main = do
+  x <- fmap lines getContents
+  putStrLn $ render $ formatsToSketch $ stringToFormats $ head x
+
+formatsToSketch :: [Format] -> Doc
+formatsToSketch formats =
+  let zipformats = zip [1..] formats in
+  vcat [ formatsToStructs zipformats
+       , fileSizeDecl formats ]
+
+-- TODO: Intermediate format that's an AST, with methods for rendering
+-- each sort of thing
+
+
+-- Struct declarations
+
+formatsToStructs :: [(Integer, Format)] -> Doc
+formatsToStructs formats =
+  vcat $ map formatToStruct formats
+
+formatToStruct :: (Integer, Format) -> Doc
+formatToStruct (index, format) =
+  block
+    (text "struct" <+> text "DataStruct_" <> integer index)
+    (vcat $ map (fieldToStructDecl . fst) format)
+
+fieldToStructDecl :: Field -> Doc
+fieldToStructDecl (IntField fieldName) =
+  text "int" <+> text "field" <> int fieldName <> semi
+
+
+-- File size declaration
+
+fileSizeDecl :: [Format] -> Doc
+fileSizeDecl formats =
+  text "int" <+> text "fileSize" <+> equals <+>
+  integer (fileSize formats) <> semi --TODO
+
+fileSize :: [Format] -> Integer
+fileSize formats =
+    maximum $ map (sum . (map fieldSize)) formats
+
+fieldSize :: (Field, RenderStrategy) -> Integer
+fieldSize (field, AttributeCode code) = 1 + fieldSize (field, code)
+fieldSize (IntField _, Raw) = 2
+
+
+-- Writer functions
+
+-- Checker functions
+
+-- Total cost declaration
+
+-- Generator bit check()
+
+-- Generator int readExp()
+
+-- Read function
+
+-- Harness void main()
+
+
+
+
+
+-- Helper functions
+
+block :: Doc -> Doc -> Doc
+block header body = vcat [ header <+> char '{'
+                         , nest 4 body
+                         , char '}' ]
+
+stringToFormats :: String -> [Format]
+stringToFormats = read
+
