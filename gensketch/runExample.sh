42d14c83fe0e4869c89b39e6c8581942c7e08327
Chelsea Voss <csvoss@mit.edu>
2015-04-17 23:57:22 -0400

    Fixed runExample.sh's filepaths

--- gensketch/runExample.sh
+++ gensketch/runExample.sh
@@ -1,4 +1,4 @@
 cabal build
 echo -e "[[(IntField 1,Raw)],[(IntField 2,AttributeCode Raw),(IntField 1,Raw)],[(IntField 2,AttributeCode Raw),(IntField 1,Raw),(IntField 3,AttributeCode Raw)]]" | ./dist/build/gensketch/gensketch > GenSketchOutput.sk
-sketch --fe-custom-codegen ~/synthetic_evolution/java_code_generator/java_code_generator.jar ~/synthetic_evolution/gensketch/GenSketchOutput.sk
+sketch --fe-custom-codegen ../java_code_generator/java_code_generator.jar ../gensketch/GenSketchOutput.sk
 
